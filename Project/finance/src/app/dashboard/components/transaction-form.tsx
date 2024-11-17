8f42c8f4aef2e6f7a697fa8978e125b2cf7369e4
Your Name <soumyajeetdas1998@gmail.com>
2024-11-17 22:39:25 +0530

    Add code

--- Project/finance/src/app/dashboard/components/transaction-form.tsx
+++ Project/finance/src/app/dashboard/components/transaction-form.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 'use client';
 
 import React, { useState } from 'react';
@@ -9,7 +12,7 @@ import Button from '@/components/button';
 import { useForm } from 'react-hook-form';
 import transactionSchema from '@/lib/validation';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
+import { undefined, z } from 'zod';
 import { createTransaction } from '@/lib/action';
 import { useRouter } from 'next/navigation';
 import FormError from '@/components/form-error';
@@ -19,27 +22,30 @@ const TransactionForm = () => {
   const {
     register,
     handleSubmit,
+    watch,
+    setValue,
     control,
     formState: { errors },
   } = useForm<z.infer<typeof transactionSchema>>({
     mode: 'onTouched',
     resolver: zodResolver(transactionSchema),
   });
-
   const router = useRouter();
-
   const [isSaving, setSaving] = useState(false);
-  const [lastError, setLastError] = useState<undefined | null | Error>(null);
+  const [lastError, setLastError] = useState<null | undefined | Error>();
+  const type = watch('type');
 
   const onSubmit = async (data: z.infer<typeof transactionSchema>) => {
+    console.log(data);
+
     setSaving(true);
+
+    setLastError(undefined);
 
     try {
       await createTransaction(data);
-
       router.push('/dashboard');
     } catch (error) {
-      // Handling errors from Server Actions
       setLastError(error as Error);
     } finally {
       setSaving(false);
@@ -48,68 +54,58 @@ const TransactionForm = () => {
 
   return (
     <>
-      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)} noValidate>
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
             <Label className="mb-1">Type</Label>
-            <Select {...register('type')}>
+            <Select
+              {...register('type', {
+                onChange: (e) => {
+                  if (e.target.value !== 'Expense') {
+                    setValue('category', undefined);
+                  }
+                },
+              })}
+            >
               {types.map((type) => (
                 <option key={type}>{type}</option>
               ))}
             </Select>
+            <FormError error={errors.type as unknown as Error} />
           </div>
 
           <div>
             <Label className="mb-1">Category</Label>
-            <Select {...register('category')}>
+            {/*@ts-ignore*/}
+            <Select {...register('category')} disabled={type !== 'Expense'}>
+              <option value="">Select a category</option>
               {categories.map((category) => (
                 <option key={category}>{category}</option>
               ))}
             </Select>
+            <FormError error={errors.category as unknown as Error} />
           </div>
 
           <div>
             <Label className="mb-1">Date</Label>
-            <Input
-              {...register('created_at', {
-                required: 'The date is required',
-              })}
-            />
-            {errors.created_at && (
-              <p className="mt-1 text-red-500">{errors.created_at.message}</p>
-            )}
+            <Input {...register('created_at')} />
+            <FormError error={errors.created_at as unknown as Error} />
           </div>
 
           <div>
             <Label className="mb-1">Amount</Label>
-            <Input
-              type="number"
-              {...register('amount', {
-                required: 'The amount is required',
-                valueAsNumber: true,
-                min: { value: 0, message: 'The amount must be greater than 0' },
-              })}
-            />
-            {errors.amount && (
-              <p className="mt-1 text-red-500">{errors.amount.message}</p>
-            )}
+            <Input type="number" {...register('amount')} />
+            <FormError error={errors.amount as unknown as Error} />
           </div>
 
-          <div className="col-span-2">
+          <div className="col-span-1 md:col-span-2">
             <Label className="mb-1">Description</Label>
-            <Input
-              {...register('description', {
-                required: 'The description is required',
-              })}
-            />
-            {errors.description && (
-              <p className="mt-1 text-red-500">{errors.description.message}</p>
-            )}
+            <Input {...register('description')} />
+            <FormError error={errors.description as unknown as Error} />
           </div>
         </div>
 
         <div className="flex justify-between items-center">
-          {/*Handling the error in form*/}
           <div>{lastError && <FormError error={lastError} />}</div>
           <Button type="submit" disabled={isSaving}>
             Save
--- Project/finance/src/lib/validation.ts
+++ Project/finance/src/lib/validation.ts
@@ -1,18 +1,50 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
 import { z } from 'zod';
 import { categories, types } from './consts';
 
-const transactionSchema = z.object({
-  type: z.enum(types as [string, ...string[]]),
-  category: z.enum(categories as [string, ...string[]]),
-  amount: z.coerce.number().min(1, {
-    message: 'Amount must be at least 1',
-  }),
-  description: z.string().min(1, {
-    message: 'The description is required',
-  }),
-  created_at: z.string().refine((val: string) => !isNaN(Date.parse(val)), {
-    message: 'Date needs to contain a valid date',
-  }),
-});
+const transactionSchema = z
+  .object({
+    type: z.enum(types as [string, ...string[]]),
+
+    // Before parsing it tranforms the value to undefined if it is an empty string or null or undefined
+    // Ref: https://zod.dev/?id=preprocess
+    category: z.preprocess(
+      // @ts-ignore
+      (val) => (val?.length ? val : undefined),
+      z.string().optional(),
+    ),
+
+    // After parsing if it finds the value anything other than number, it will convert that into a number
+    // Ref : https://zod.dev/?id=coercion-for-primitives
+    amount: z.coerce.number().min(1, {
+      message: 'Amount must be at least 1',
+    }),
+    description: z.string().optional(),
+
+    // Refine in field level
+    created_at: z.string().refine((val: string) => !isNaN(Date.parse(val)), {
+      message: 'Date needs to contain a valid date',
+    }),
+  })
+
+  // Refine in schema level. when we want to access multiple field for validation
+  .refine(
+    (data) => {
+      // Category is required when type is Expense
+      if (data.type === 'Expense') {
+        return (
+          data.category !== undefined && categories.includes(data.category)
+        );
+      }
+
+      // Validation not required for category when type is not Expense
+      return true;
+    },
+    {
+      path: ['category'],
+      message: 'Category is required for Expense',
+    },
+  );
 
 export default transactionSchema;
